e2ba2f331c2b34ad96502c1bf5ff0d63d5bb1404
Suresh <surdhana@in.ibm.com>
2022-01-28 20:43:59 +0530

    tweaks

--- main.js
+++ main.js
@@ -17,11 +17,8 @@ const { left, top } = canvas.getBoundingClientRect();
 const ctx = canvas.getContext('2d');
 const stepWidthFactor = 100;
 let score = 0;
+let prevPos = 'left';
 
-ctx.globalCompositeOperation = "source-over";
-ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.globalCompositeOperation = "lighter";
 
 function getRandomColor() {
   var r = Math.random() * 255 >> 0;
@@ -33,8 +30,8 @@ function getRandomColor() {
 // player
 class Player {
   constructor() {
-    this.color = "red"
-    this.radius = 50
+    this.color = white
+    this.radius = 10
     this.position = {
       x: canvas.width / 2,
       y: canvas.height / 2
@@ -46,12 +43,7 @@ class Player {
     ctx.save()
     ctx.globalAlpha = this.opacity
     ctx.beginPath();
-    const gradient = ctx.createRadialGradient(this.position.x, this.position.y, 0, this.position.x, this.position.y, this.radius);
-    gradient.addColorStop(0, white);
-    gradient.addColorStop(0.4, white);
-    gradient.addColorStop(0.4, this.color);
-    gradient.addColorStop(1, "black");
-    ctx.fillStyle = gradient;
+    ctx.fillStyle = this.color;
     ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
     ctx.fill()
     ctx.closePath()
@@ -82,7 +74,7 @@ class Player {
 // projectile
 class Projectile {
   constructor({ position, target, color, radius }) {
-    this.radius = radius || 12;
+    this.radius = radius || 6;
     this.color = color || white;
     this.position = {
       x: position.x,
@@ -123,28 +115,27 @@ class Projectile {
 }
 
 class Enemy {
-  constructor({ position, velocity, color, radius }) {
+  constructor({ position, color, radius }) {
+    this.radius = radius;
+    this.color = color || white;
     this.position = {
       x: position.x,
       y: position.y
     }
-    this.velocity = {
-      x: velocity.x,
-      y: velocity.y
+    this.target = {
+      x: player.position.x,
+      y: player.position.y
     }
-    this.radius = radius
-    this.color = color
-    this.projectiles = []
+    this.velocity = {
+      x: (this.position.x - this.target.x) / 500 * -1,
+      y: (this.position.y - this.target.y) / 500 * -1
+    }
+    this.opacity = 1
   }
   draw() {
     ctx.save()
     ctx.globalAlpha = this.opacity
-    const gradient = ctx.createRadialGradient(this.position.x, this.position.y, 0, this.position.x, this.position.y, this.radius);
-    gradient.addColorStop(0, white);
-    gradient.addColorStop(0.4, white);
-    gradient.addColorStop(0.4, this.color);
-    gradient.addColorStop(1, "black");
-    ctx.fillStyle = gradient;
+    ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
     ctx.fill()
@@ -153,18 +144,15 @@ class Enemy {
   }
   update() {
     this.draw()
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-  }
-  shoot() {
-    this.projectiles.push(new Projectile({
-      position: this.position,
-      color: "yellow",
-      target: {
-        x: player.position.x,
-        y: player.position.y
-      }
-    }))
+    const shouldMove = Math.abs(this.position.x - this.target.x) > 1 || Math.abs(this.position.y - this.target.y) > 1;
+    if (shouldMove) {
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+    } else {
+      this.velocity.x = 0;
+      this.velocity.y = 0;
+      this.opacity = 0
+    }
   }
   hide(particles) {
     this.opacity = 0;
@@ -180,6 +168,7 @@ class Enemy {
   }
 }
 
+const friction = 0.99
 class Particle {
   constructor({ position, color }) {
     this.position = {
@@ -206,13 +195,11 @@ class Particle {
   }
   update() {
     this.draw()
+    this.velocity.x *= friction
+    this.velocity.y *= friction
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
-    if (this.opacity > 0) {
-      this.opacity -= 0.05
-    } else {
-      this.opacity = 0;
-    }
+    this.opacity -= 0.01
   }
 }
 
@@ -221,76 +208,63 @@ const player = new Player();
 const enemies = [];
 const particles = [];
 let frames = 0;
-let enemySpawnGap = 1;
 
 function animate() {
   if (!game.active) return;
   requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
 
   // update enemy position and remove if it is out of canvas already
   enemies.forEach((enemy, enemyIndex) => {
-    if (enemy.position.x + enemy.radius >= canvas.width || enemy.position.x - enemy.radius <= 0 ||
-      enemy.position.y + enemy.radius >= canvas.height || enemy.position.y - enemy.radius <= 0) {
+
+    // update enemy projectiles position and remove if it faded already
+    if (enemy.position.x + enemy.radius >= player.position.x + player.radius &&
+      enemy.position.x - enemy.radius <= player.position.x - player.radius &&
+      enemy.position.y + enemy.radius >= player.position.y + player.radius &&
+      enemy.position.y - enemy.radius <= player.position.y - player.radius) {
+      showResult('you lose')
+      game.over = true
+      if (player.opacity > 0) player.hide(particles);
       setTimeout(() => {
-        enemies.splice(enemyIndex, 1)
-      }, 0)
+        game.active = false;
+      }, 3000)
     } else {
-      enemy.update()
-
-      // update enemy projectiles position and remove if it faded already
-      enemy.projectiles.forEach((eProjectile, eProjectileIndex) => {
-        if (eProjectile.position.x + eProjectile.radius <= player.position.x + player.radius &&
-          eProjectile.position.x - eProjectile.radius >= player.position.x - player.radius &&
-          eProjectile.position.y + eProjectile.radius <= player.position.y + player.radius &&
-          eProjectile.position.y - eProjectile.radius >= player.position.y - player.radius) {
-          showResult('you lose')
-          game.over = true
-          player.hide(particles);
-          setTimeout(() => {
-            game.active = false;
-          }, 3000)
-        } else if (eProjectile.opacity === 0) {
-          setTimeout(() => {
-            enemy.projectiles.splice(eProjectileIndex, 1)
-          }, 0)
-        } else {
-          eProjectile.update();
-        }
-      })
-
-      // detect collision with player projectile on enemy
-      player.projectiles.forEach((playerProjectile, playerProjectileIndex) => {
-        if (enemy.position.x + enemy.radius >= playerProjectile.position.x + playerProjectile.radius &&
-          enemy.position.x - enemy.radius <= playerProjectile.position.x - playerProjectile.radius &&
-          enemy.position.y + enemy.radius >= playerProjectile.position.y + playerProjectile.radius &&
-          enemy.position.y - enemy.radius <= playerProjectile.position.y - playerProjectile.radius) {
-          enemy.hide(particles);
-          updateScore();
-          setTimeout(() => {
-            const playerProjectileFound = player.projectiles.find(i => i === playerProjectile);
-            if (playerProjectileFound) player.projectiles.splice(playerProjectileIndex, 1)
-            const enemyFound = enemies.find(j => j === enemy);
-            if (enemyFound) enemies.splice(enemyIndex, 1)
-          }, 0);
-        } else if (playerProjectile.opacity === 0) {
-          // remove if it faded already
-          setTimeout(() => {
-            player.projectiles.splice(playerProjectileIndex, 1)
-          }, 0)
-        } else {
-          // update player projectiles position
-          playerProjectile.update();
-        }
-      })
-
+      enemy.update();
     }
+
+    // detect collision with player projectile on enemy
+    player.projectiles.forEach((playerProjectile, playerProjectileIndex) => {
+      if (enemy.position.x + enemy.radius >= playerProjectile.position.x + playerProjectile.radius &&
+        enemy.position.x - enemy.radius <= playerProjectile.position.x - playerProjectile.radius &&
+        enemy.position.y + enemy.radius >= playerProjectile.position.y + playerProjectile.radius &&
+        enemy.position.y - enemy.radius <= playerProjectile.position.y - playerProjectile.radius) {
+        enemy.hide(particles);
+        updateScore();
+        setTimeout(() => {
+          const playerProjectileFound = player.projectiles.find(i => i === playerProjectile);
+          if (playerProjectileFound) player.projectiles.splice(playerProjectileIndex, 1)
+          const enemyFound = enemies.find(j => j === enemy);
+          if (enemyFound) enemies.splice(enemyIndex, 1)
+        }, 0);
+      } else if (playerProjectile.opacity === 0) {
+        // remove if it faded already
+        setTimeout(() => {
+          player.projectiles.splice(playerProjectileIndex, 1)
+        }, 0)
+      } else {
+        // update player projectiles position
+        playerProjectile.update();
+      }
+    })
+
+
   })
 
   // update particle position and remove if it faded already
   particles.forEach((particle, particleIndex) => {
-    if (particle.opacity === 0) {
+    if (particle.opacity <= 0) {
       setTimeout(() => {
         particles.splice(particleIndex, 1)
       }, 0)
@@ -300,30 +274,16 @@ function animate() {
   })
 
   // generate enemy 
-  if (frames % 10 === 0) {
-    enemies.push(new Enemy({
-      position: {
-        x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height
-      },
-      color: getRandomColor(),
-      radius: (Math.random() * 10) + 20,
-      velocity: {
-        x: Math.round((Math.random() * -10) + 5),
-        y: Math.round((Math.random() * -10) + 5)
-      }
-    }))
-  }
-
-  // spawn projectiles from random enemy
-  if (enemySpawnGap % 1000 === 0) {
-    enemies[Math.floor(Math.random() * enemies.length)].shoot();
-    enemySpawnGap = 1;
+  if (frames % 30 === 0) {
+    spwanEnimies()
   }
 
   if (score >= 1000) {
     game.over = true
-    enemies.forEach(enemy => enemy.hide(particles))
+    enemies.forEach((enemy, index) => {
+      enemy.hide(particles)
+      enemies.splice(index, 1)
+    })
     setTimeout(() => {
       game.active = false;
     }, 1000)
@@ -331,10 +291,48 @@ function animate() {
   }
 
   frames++
-  enemySpawnGap++
 }
 
 animate();
+
+function getRandomPos(radius) {
+  let random = Math.random(), posX, posY, pos;
+  if (random <= 0.25) {
+    posX = 0 - radius
+    posY = Math.random() * canvas.height - radius
+    pos = "top"
+  } else if (random >= 0.26 && random <= 0.5) {
+    posX = Math.random() * canvas.width - radius
+    posY = 0 - radius
+    pos = "left"
+  } else if (random >= 0.51 && random <= 0.75) {
+    posX = canvas.width + radius
+    posY = Math.random() * canvas.height - radius
+    pos = "right"
+  } else {
+    posX = Math.random() * canvas.width - radius
+    posY = canvas.height + radius
+    pos = "bottom"
+  }
+  if (prevPos === pos) {
+    return getRandomPos(radius)
+  }
+  prevPos = pos
+  return { x: posX, y: posY }
+}
+
+
+function spwanEnimies() {
+  let radius = Math.floor(Math.random() * 30) + 10
+  const { x, y } = getRandomPos(radius)
+  enemies.push(new Enemy({
+    position: {
+      x, y
+    },
+    color: getRandomColor(),
+    radius: radius
+  }))
+}
 
 function mouseDownFn({ clientX, clientY, preventDefault }) {
   if (game.over) return
